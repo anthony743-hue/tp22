8e7cb8b65a1d2bac269be14b06008fb8f91fed07
faniry74 <faniryanthony74@gmail.com>
2025-06-30 13:29:16 +0300

    test

--- pages/traitement_recherche.php
+++ pages/traitement_recherche.php
@@ -6,14 +6,3 @@ $min = $_POST['min'];
 $max = $_POST['max'];
 $emp = getRelative_Employees($dep,$name, $min, $max);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
